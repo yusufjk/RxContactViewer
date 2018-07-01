3646689c21068ed14995973228698adc6dce34f1
Yusuf Jk <yjkheraluwala@gmail.com>
2018-07-01 16:32:03 +0530

    added contact wrapper class

---
+++ app/src/main/java/com/rxcontactviewer/network/model/ContactWrapper.java
@@ -0,0 +1,9 @@
+package com.rxcontactviewer.network.model;
+
+public class ContactWrapper {
+    private Contact[] contacts;
+
+    public Contact[] getContacts() {
+        return contacts;
+    }
+}
