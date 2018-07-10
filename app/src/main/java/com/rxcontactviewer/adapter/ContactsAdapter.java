48a82755f24e5dbab46a1f9e47d36bf3d46eb88d
yusufjk <yusuf.k@evivehealth.com>
2018-07-10 14:58:28 +0530

    added search implementation

--- app/src/main/java/com/rxcontactviewer/adapter/ContactsAdapter.java
+++ app/src/main/java/com/rxcontactviewer/adapter/ContactsAdapter.java
@@ -5,32 +5,69 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
+import com.google.common.base.Strings;
 import com.rxcontactviewer.R;
 import com.rxcontactviewer.network.model.Contact;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.MyViewHolder> {
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.MyViewHolder> implements Filterable {
     private Context context;
     private List<Contact> contactList;
+    private Object contactListFiltered;
+    private ContactsAdapterListener listener;
 
-    public ContactsAdapter(Context context, List<Contact> contactList) {
+    public ContactsAdapter(Context context, List<Contact> contactList, ContactsAdapterListener listener) {
         this.context = context;
         this.contactList = contactList;
+        this.listener = listener;
+        this.contactListFiltered = contactList;
     }
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, final int position) {
         Contact contact = contactList.get(position);
-
+        List<String> phones = contact.getPhones();
+        List<String> managers = contact.getManagers();
+        List<String> addresses = contact.getAddresses();
         holder.name.setText(contact.getName());
         holder.companyName.setText(contact.getCompanyName());
-        //  holder.phones.setText(contact.getPhones().get(position));
-        // holder.managers.setText(contact.getManagers().get(position));
         holder.parent.setText(contact.getParent());
-        // holder.addresses.setText(contact.getAddresses().get(position));
+        nestedArrays(holder, phones, managers, addresses);
+    }
+
+    private void nestedArrays(MyViewHolder holder, List<String> phones, List<String> managers, List<String> addresses) {
+        if (null == phones) {
+            holder.phones.setText("-");
+        } else {
+            for (int i = 0; i < phones.size(); i++) {
+                holder.phones.setText(phones.get(i));
+            }
+
+        }
+
+        if (null == managers) {
+            holder.managers.setText("-");
+        } else {
+
+            for (int i = 0; i < managers.size(); i++) {
+                holder.managers.setText(managers.get(i));
+            }
+        }
+
+        if (null == addresses) {
+            holder.addresses.setText("-");
+        } else {
+
+            for (int i = 0; i < addresses.size(); i++) {
+                holder.addresses.setText(addresses.get(i));
+            }
+        }
 
     }
 
@@ -47,6 +84,41 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.MyView
         return contactList.size();
     }
 
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String charString = charSequence.toString();
+                if (charString.isEmpty()) {
+                    contactListFiltered = contactList;
+                }
+                List<Contact> filteredList = new ArrayList<>();
+                for (int i = 0; i < contactList.size(); i++) {
+                    Contact contact = contactList.get(i);
+                    if (!Strings.isNullOrEmpty(contact.getName()) && contact.getName().toLowerCase().contains(charString.toLowerCase())) {
+                        filteredList.add(contact);
+                        contactListFiltered = filteredList;
+                    }
+                }
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = contactListFiltered;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                contactListFiltered = filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
+
+    public interface ContactsAdapterListener {
+        void onContactSelected(Contact contact);
+    }
+
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView name, phones, companyName, managers, addresses, parent;
 
@@ -58,6 +130,13 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.MyView
             managers = view.findViewById(R.id.managers);
             addresses = view.findViewById(R.id.addresses);
             parent = view.findViewById(R.id.parent);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // send selected contact in callback
+                    listener.onContactSelected(contactList.get(getAdapterPosition()));
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/rxcontactviewer/view/RemoteSearchActivity.java
+++ app/src/main/java/com/rxcontactviewer/view/RemoteSearchActivity.java
@@ -9,10 +9,13 @@ import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
+import com.jakewharton.rxbinding2.widget.RxTextView;
+import com.jakewharton.rxbinding2.widget.TextViewTextChangeEvent;
 import com.rxcontactviewer.R;
 import com.rxcontactviewer.adapter.ContactsAdapter;
 import com.rxcontactviewer.network.ApiClient;
@@ -23,6 +26,7 @@ import com.rxcontactviewer.network.model.ContactList;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -30,10 +34,12 @@ import butterknife.Unbinder;
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
 
-public class RemoteSearchActivity extends AppCompatActivity {
+public class RemoteSearchActivity extends AppCompatActivity implements ContactsAdapter.ContactsAdapterListener {
 
     private static final String TAG = RemoteSearchActivity.class.getSimpleName();
     @BindView(R.id.input_search)
@@ -42,6 +48,7 @@ public class RemoteSearchActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     private ApiService apiService;
     private ContactsAdapter mAdapter;
+    private CompositeDisposable disposable = new CompositeDisposable();
     private List<Contact> contactsList = new ArrayList<>();
     private Unbinder unbinder;
 
@@ -51,13 +58,13 @@ public class RemoteSearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_remote_search);
         unbinder = ButterKnife.bind(this);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        final Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         }
 
-        mAdapter = new ContactsAdapter(this, contactsList);
+        mAdapter = new ContactsAdapter(this, contactsList, this);
 
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(mLayoutManager);
@@ -67,42 +74,18 @@ public class RemoteSearchActivity extends AppCompatActivity {
         whiteNotificationBar(recyclerView);
 
         apiService = ApiClient.getClient().create(ApiService.class);
-        Observable<ContactList> contactObservable = apiService.getContacts().subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread());
-        contactObservable.subscribe(new Observer<ContactList>() {
-            @Override
-            public void onSubscribe(Disposable d) {
 
-            }
 
-            @Override
-            public void onNext(ContactList contactList) {
-                List<Contact> contacts = contactList.getContactList();
-                for (int i = 0; i < contacts.size(); i++) {
-                    Contact contact = new Contact();
-                    contact.setName(contacts.get(i).getName());
-                    contact.setCompanyName(contacts.get(i).getCompanyName());
-                    contact.setPhones(contacts.get(i).getPhones());
-                    contact.setParent(contacts.get(i).getParent());
-                    contact.setManagers(contacts.get(i).getManagers());
-                    contact.setAddresses(contacts.get(i).getAddresses());
-                    contactsList.add(contact);
-                }
+        disposable.add(RxTextView.textChangeEvents(inputSearch)
+                .skipInitialValue()
+                .debounce(300, TimeUnit.MILLISECONDS)
+                .distinctUntilChanged()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(searchContacts()));
 
-                mAdapter = new ContactsAdapter(RemoteSearchActivity.this, contactsList);
-                recyclerView.setAdapter(mAdapter);
-            }
+        fetchContacts();
 
-            @Override
-            public void onError(Throwable e) {
-
-            }
-
-            @Override
-            public void onComplete() {
-
-            }
-        });
 
     }
 
@@ -129,4 +112,68 @@ public class RemoteSearchActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onContactSelected(Contact contact) {
+
+    }
+
+    private DisposableObserver<TextViewTextChangeEvent> searchContacts() {
+        return new DisposableObserver<TextViewTextChangeEvent>() {
+            @Override
+            public void onNext(TextViewTextChangeEvent textViewTextChangeEvent) {
+                Log.d(TAG, "Search query: " + textViewTextChangeEvent.text());
+                mAdapter.getFilter().filter(textViewTextChangeEvent.text());
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+    }
+
+    private void fetchContacts() {
+        Observable<ContactList> contactObservable = apiService.getContacts().subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread());
+        contactObservable.subscribe(new Observer<ContactList>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(ContactList contactList) {
+                List<Contact> contacts = contactList.getContactList();
+                for (int i = 0; i < contacts.size(); i++) {
+                    Contact contact = new Contact();
+                    contact.setName(contacts.get(i).getName());
+                    contact.setCompanyName(contacts.get(i).getCompanyName());
+                    contact.setPhones(contacts.get(i).getPhones());
+                    contact.setParent(contacts.get(i).getParent());
+                    contact.setManagers(contacts.get(i).getManagers());
+                    contact.setAddresses(contacts.get(i).getAddresses());
+                    contactsList.add(contact);
+                }
+
+                recyclerView.setAdapter(mAdapter);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+    }
+
 }
